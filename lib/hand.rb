85c5abb1b8bcdf80dfbb3fee028e682452c33a5a
Alexa Bagnard <abagnard@gmail.com>
2016-07-20 18:07:38 -0700

    finished is_straight? and is_flush? methods

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,9 +1,73 @@
-
+require 'byebug'
 
 class Hand
 
-  def initialize()
+  attr_accessor :cards
+
+  HAND_TYPE = {
+    'royal_flush' => 10,
+    'straight_flush' => 9,
+    'four_of_a_kind' => 8,
+    'full_house' => 7,
+    'flush' => 6,
+    'straight' => 5,
+    'three_of_a_kind' => 4,
+    'two_pair' => 3,
+    'pair' => 2,
+    'high_card' => 1
+  }
+
+  def initialize(cards, value = nil)
     @cards = cards
+    @value = value
   end
+
+  def check_type
+    # value = [type, val_1, val_2, val_3 ... ]
+    value = Array.new(6) { 0 }
+    # iterate through all types, either store and return value
+    # or move on to next type
+  end
+
+  def is_straight?
+    return true if @cards.map {|card| card.value }.sort == [2,3,4,5,14]
+    @cards.sort[0..-2].each_with_index do |card, i|
+      return false unless card.value + 1 == @cards[i + 1].value
+    end
+    true
+  end
+
+  def is_flush?
+    @cards.map { |card| card.suit }.uniq == 1
+  end
+
+  def is_straight_flush?
+    is_flush? && is_straight?
+  end
+
+  def is_royal_flush?
+    is_flush? && @cards.map {|card| card.value }.sort == [10, 11, 12, 13, 14]
+  end
+
+  def is_four_of_a_kind?
+  end
+
+  def is_three_of_a_kind?
+  end
+
+  def is_two_of_a_kind?
+  end
+
+  def is_two_pair?
+  end
+
+  def is_full_house?
+  end
+
+  def is_high_card?
+  end
+
+
+
 
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,9 +1,47 @@
 require 'hand.rb'
 
-describe Deck do
+describe Hand do
+
+
+  let(:straight_1) { Hand.new([
+    Card.new('10', 'H'),
+    Card.new('J', 'D'),
+    Card.new('Q', 'C'),
+    Card.new('K', 'S'),
+    Card.new('A', 'H')
+  ]) }
+
+  let(:straight_flush) { Hand.new([
+    Card.new('A', 'H'),
+    Card.new('5', 'H'),
+    Card.new('3', 'H'),
+    Card.new('2', 'H'),
+    Card.new('4', 'H')
+  ]) }
+
+  let(:not_straight) { Hand.new([
+    Card.new('10', 'H'),
+    Card.new('J', 'D'),
+    Card.new('5', 'C'),
+    Card.new('K', 'S'),
+    Card.new('A', 'H')
+  ]) }
+
   describe "#initialize" do
-    it "" do
-      
+    it ""
+  end
+
+  describe '#is_straight?' do
+    it 'recognizes only straights' do
+      expect(straight_1.is_straight?).to be true
+      expect(straight_flush.is_straight?).to be true
+      expect(not_straight.is_straight?).to be false
     end
   end
+
+  describe '#is_flush?' do
+    expect(straight_flush.is_flush?).to be true
+    expect(straight_1.is_flush?).to be false
+  end
+
 end
